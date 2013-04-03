a3d55ffd00757526e1995d2e51e98af2d034a7f7
Jeremy Frazao <git@frazao.net>
2013-04-02 23:29:38 -0600

    initial commit

---
+++ app/controllers/temp_controller.rb
@@ -0,0 +1,7 @@
+class TempController < ApplicationController
+
+  def index
+    
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookoneer::Application.config.secret_token = 'dd3e46a827e995c2cce8ca0997fb3e3e255e4f59306126435895e7e73cc0a3e83df747638355cc9b75cc1ee3a83f99ccb5c6e8ee5f2a5b444159e33676bc5106'
